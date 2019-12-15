d64547f7b39efcc115448993e8cd00339ff190e4
DlhSoft Team <Contact@DlhSoft.com>
2019-12-15 11:04:07 +0200

    Sample

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace GanttChartLightLibraryCoreDemo
+namespace GanttChartDataGridSample
 {
     /// <summary>
     /// Interaction logic for App.xaml
